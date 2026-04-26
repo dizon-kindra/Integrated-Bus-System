321d9e99449ad162737b7d4f77860ac74097d05e
dizon-kindra <dizonkinkin28@gamil.com>
2026-04-26 10:43:37 +0800

    Update Form1 design and functionality

--- Bus-Terminal-Management-System/bus-main/sr/Form1.Designer.cs
+++ Bus-Terminal-Management-System/bus-main/sr/Form1.Designer.cs
@@ -1,136 +1,231 @@
-﻿
-namespace sr
+﻿namespace sr
 {
     partial class Form1
     {
-        /// <summary>
-        /// Required designer variable.
-        /// </summary>
         private System.ComponentModel.IContainer components = null;
 
-        /// <summary>
-        /// Clean up any resources being used.
-        /// </summary>
-        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        private System.Windows.Forms.Timer timer1;
+        private Bunifu.UI.WinForms.BunifuGradientPanel bunifuGradientPanel1;
+        private System.Windows.Forms.Panel panelContent;
+        private System.Windows.Forms.Panel panelLeft;
+        private System.Windows.Forms.Panel panelRight;
+        private System.Windows.Forms.PictureBox pictureBoxBus;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label lblLoading;
+        private CircularProgressBar.CircularProgressBar progressbar;
+
         protected override void Dispose(bool disposing)
         {
             if (disposing && (components != null))
             {
                 components.Dispose();
             }
+
             base.Dispose(disposing);
         }
 
-        #region Windows Form Designer generated code
-
-        /// <summary>
-        /// Required method for Designer support - do not modify
-        /// the contents of this method with the code editor.
-        /// </summary>
         private void InitializeComponent()
         {
             this.components = new System.ComponentModel.Container();
-            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
             this.timer1 = new System.Windows.Forms.Timer(this.components);
             this.bunifuGradientPanel1 = new Bunifu.UI.WinForms.BunifuGradientPanel();
+            this.panelContent = new System.Windows.Forms.Panel();
+            this.panelLeft = new System.Windows.Forms.Panel();
+            this.pictureBoxBus = new System.Windows.Forms.PictureBox();
+            this.panelRight = new System.Windows.Forms.Panel();
+            this.lblLoading = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
             this.label1 = new System.Windows.Forms.Label();
             this.progressbar = new CircularProgressBar.CircularProgressBar();
+
             this.bunifuGradientPanel1.SuspendLayout();
+            this.panelContent.SuspendLayout();
+            this.panelLeft.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxBus)).BeginInit();
+            this.panelRight.SuspendLayout();
             this.SuspendLayout();
+
             // 
             // timer1
             // 
             this.timer1.Enabled = true;
+            this.timer1.Interval = 35;
             this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+
             // 
             // bunifuGradientPanel1
             // 
             this.bunifuGradientPanel1.BackColor = System.Drawing.Color.Transparent;
-            this.bunifuGradientPanel1.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("bunifuGradientPanel1.BackgroundImage")));
             this.bunifuGradientPanel1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
             this.bunifuGradientPanel1.BorderRadius = 1;
-            this.bunifuGradientPanel1.Controls.Add(this.label1);
-            this.bunifuGradientPanel1.Controls.Add(this.progressbar);
+            this.bunifuGradientPanel1.Controls.Add(this.panelContent);
             this.bunifuGradientPanel1.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.bunifuGradientPanel1.GradientBottomLeft = System.Drawing.Color.DarkViolet;
-            this.bunifuGradientPanel1.GradientBottomRight = System.Drawing.Color.Teal;
-            this.bunifuGradientPanel1.GradientTopLeft = System.Drawing.Color.RoyalBlue;
-            this.bunifuGradientPanel1.GradientTopRight = System.Drawing.Color.BlueViolet;
+            this.bunifuGradientPanel1.GradientBottomLeft = System.Drawing.Color.FromArgb(11, 24, 38);
+            this.bunifuGradientPanel1.GradientBottomRight = System.Drawing.Color.FromArgb(19, 58, 84);
+            this.bunifuGradientPanel1.GradientTopLeft = System.Drawing.Color.FromArgb(22, 34, 50);
+            this.bunifuGradientPanel1.GradientTopRight = System.Drawing.Color.FromArgb(52, 152, 219);
             this.bunifuGradientPanel1.Location = new System.Drawing.Point(0, 0);
             this.bunifuGradientPanel1.Name = "bunifuGradientPanel1";
             this.bunifuGradientPanel1.Quality = 10;
-            this.bunifuGradientPanel1.Size = new System.Drawing.Size(874, 380);
+            this.bunifuGradientPanel1.Size = new System.Drawing.Size(1000, 500);
             this.bunifuGradientPanel1.TabIndex = 0;
             this.bunifuGradientPanel1.Click += new System.EventHandler(this.bunifuGradientPanel1_Click);
+
+            // 
+            // panelContent
+            // 
+            this.panelContent.BackColor = System.Drawing.Color.Transparent;
+            this.panelContent.Controls.Add(this.panelLeft);
+            this.panelContent.Controls.Add(this.panelRight);
+            this.panelContent.Location = new System.Drawing.Point(35, 35);
+            this.panelContent.Name = "panelContent";
+            this.panelContent.Size = new System.Drawing.Size(930, 430);
+            this.panelContent.TabIndex = 0;
+
+            // 
+            // panelLeft
+            // 
+            this.panelLeft.BackColor = System.Drawing.Color.FromArgb(245, 247, 250);
+            this.panelLeft.Controls.Add(this.pictureBoxBus);
+            this.panelLeft.Location = new System.Drawing.Point(0, 0);
+            this.panelLeft.Name = "panelLeft";
+            this.panelLeft.Size = new System.Drawing.Size(535, 430);
+            this.panelLeft.TabIndex = 0;
+
+            // 
+            // pictureBoxBus
+            // 
+            this.pictureBoxBus.BackColor = System.Drawing.Color.White;
+            this.pictureBoxBus.Location = new System.Drawing.Point(25, 35);
+            this.pictureBoxBus.Name = "pictureBoxBus";
+            this.pictureBoxBus.Size = new System.Drawing.Size(485, 360);
+            this.pictureBoxBus.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBoxBus.TabIndex = 0;
+            this.pictureBoxBus.TabStop = false;
+            this.pictureBoxBus.Click += new System.EventHandler(this.pictureBoxBus_Click);
+
+            // 
+            // panelRight
+            // 
+            this.panelRight.BackColor = System.Drawing.Color.FromArgb(22, 34, 50);
+            this.panelRight.Controls.Add(this.lblLoading);
+            this.panelRight.Controls.Add(this.label3);
+            this.panelRight.Controls.Add(this.label2);
+            this.panelRight.Controls.Add(this.label1);
+            this.panelRight.Controls.Add(this.progressbar);
+            this.panelRight.Location = new System.Drawing.Point(535, 0);
+            this.panelRight.Name = "panelRight";
+            this.panelRight.Size = new System.Drawing.Size(395, 430);
+            this.panelRight.TabIndex = 1;
+
             // 
             // label1
             // 
-            this.label1.AutoSize = true;
-            this.label1.Font = new System.Drawing.Font("Microsoft YaHei", 27.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label1.ForeColor = System.Drawing.SystemColors.ButtonFace;
-            this.label1.Location = new System.Drawing.Point(256, 9);
+            this.label1.Font = new System.Drawing.Font("Segoe UI", 25F, System.Drawing.FontStyle.Bold);
+            this.label1.ForeColor = System.Drawing.Color.White;
+            this.label1.Location = new System.Drawing.Point(30, 45);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(339, 48);
-            this.label1.TabIndex = 6;
-            this.label1.Text = "Welcome to SRTC";
+            this.label1.Size = new System.Drawing.Size(335, 55);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Welcome to BTMS";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+
+            // 
+            // label2
+            // 
+            this.label2.Font = new System.Drawing.Font("Segoe UI", 13F, System.Drawing.FontStyle.Bold);
+            this.label2.ForeColor = System.Drawing.Color.FromArgb(52, 152, 219);
+            this.label2.Location = new System.Drawing.Point(25, 108);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(345, 35);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Bus Terminal Management System";
+            this.label2.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+
+            // 
+            // label3
+            // 
+            this.label3.Font = new System.Drawing.Font("Segoe UI", 10.5F);
+            this.label3.ForeColor = System.Drawing.Color.Gainsboro;
+            this.label3.Location = new System.Drawing.Point(45, 150);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(305, 55);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Loading system modules, schedules, reservations, payments, and reports...";
+            this.label3.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+
             // 
             // progressbar
             // 
             this.progressbar.AnimationFunction = WinFormAnimation.KnownAnimationFunctions.Liner;
             this.progressbar.AnimationSpeed = 500;
             this.progressbar.BackColor = System.Drawing.Color.Transparent;
-            this.progressbar.Font = new System.Drawing.Font("Microsoft Sans Serif", 21.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.progressbar.Font = new System.Drawing.Font("Segoe UI", 22F, System.Drawing.FontStyle.Bold);
             this.progressbar.ForeColor = System.Drawing.Color.White;
-            this.progressbar.InnerColor = System.Drawing.Color.FromArgb(((int)(((byte)(37)))), ((int)(((byte)(42)))), ((int)(((byte)(64)))));
+            this.progressbar.InnerColor = System.Drawing.Color.FromArgb(22, 34, 50);
             this.progressbar.InnerMargin = 2;
             this.progressbar.InnerWidth = -1;
-            this.progressbar.Location = new System.Drawing.Point(377, 242);
+            this.progressbar.Location = new System.Drawing.Point(125, 225);
             this.progressbar.MarqueeAnimationSpeed = 2000;
             this.progressbar.Name = "progressbar";
-            this.progressbar.OuterColor = System.Drawing.Color.FromArgb(((int)(((byte)(24)))), ((int)(((byte)(30)))), ((int)(((byte)(54)))));
+            this.progressbar.OuterColor = System.Drawing.Color.FromArgb(12, 24, 35);
             this.progressbar.OuterMargin = -25;
             this.progressbar.OuterWidth = 26;
-            this.progressbar.ProgressColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(126)))), ((int)(((byte)(249)))));
+            this.progressbar.ProgressColor = System.Drawing.Color.FromArgb(52, 152, 219);
             this.progressbar.ProgressWidth = 10;
-            this.progressbar.SecondaryFont = new System.Drawing.Font("Microsoft Sans Serif", 36F);
-            this.progressbar.Size = new System.Drawing.Size(145, 116);
+            this.progressbar.SecondaryFont = new System.Drawing.Font("Segoe UI", 36F);
+            this.progressbar.Size = new System.Drawing.Size(145, 145);
             this.progressbar.StartAngle = 270;
             this.progressbar.Style = System.Windows.Forms.ProgressBarStyle.Continuous;
-            this.progressbar.SubscriptColor = System.Drawing.Color.FromArgb(((int)(((byte)(166)))), ((int)(((byte)(166)))), ((int)(((byte)(166)))));
+            this.progressbar.SubscriptColor = System.Drawing.Color.Gainsboro;
             this.progressbar.SubscriptMargin = new System.Windows.Forms.Padding(10, -35, 0, 0);
             this.progressbar.SubscriptText = "";
-            this.progressbar.SuperscriptColor = System.Drawing.Color.FromArgb(((int)(((byte)(166)))), ((int)(((byte)(166)))), ((int)(((byte)(166)))));
+            this.progressbar.SuperscriptColor = System.Drawing.Color.Gainsboro;
             this.progressbar.SuperscriptMargin = new System.Windows.Forms.Padding(10, 35, 0, 0);
             this.progressbar.SuperscriptText = "";
-            this.progressbar.TabIndex = 5;
+            this.progressbar.TabIndex = 3;
+            this.progressbar.Text = "0%";
             this.progressbar.TextMargin = new System.Windows.Forms.Padding(8, 8, 0, 0);
-            this.progressbar.Value = 68;
+            this.progressbar.Value = 0;
+
+            // 
+            // lblLoading
+            // 
+            this.lblLoading.Font = new System.Drawing.Font("Segoe UI", 10F, System.Drawing.FontStyle.Italic);
+            this.lblLoading.ForeColor = System.Drawing.Color.FromArgb(190, 205, 220);
+            this.lblLoading.Location = new System.Drawing.Point(45, 378);
+            this.lblLoading.Name = "lblLoading";
+            this.lblLoading.Size = new System.Drawing.Size(305, 25);
+            this.lblLoading.TabIndex = 4;
+            this.lblLoading.Text = "Please wait...";
+            this.lblLoading.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(37)))), ((int)(((byte)(42)))), ((int)(((byte)(76)))));
-            this.ClientSize = new System.Drawing.Size(874, 380);
+            this.BackColor = System.Drawing.Color.FromArgb(22, 34, 50);
+            this.ClientSize = new System.Drawing.Size(1000, 500);
             this.Controls.Add(this.bunifuGradientPanel1);
             this.ForeColor = System.Drawing.SystemColors.ControlText;
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
             this.Name = "Form1";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
-            this.Text = "Form1";
+            this.Text = "BTMS Loading";
             this.Load += new System.EventHandler(this.Form1_Load);
-            this.bunifuGradientPanel1.ResumeLayout(false);
-            this.bunifuGradientPanel1.PerformLayout();
-            this.ResumeLayout(false);
 
+            this.panelRight.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxBus)).EndInit();
+            this.panelLeft.ResumeLayout(false);
+            this.panelContent.ResumeLayout(false);
+            this.bunifuGradientPanel1.ResumeLayout(false);
+            this.ResumeLayout(false);
         }
 
-        #endregion
 
-        private System.Windows.Forms.Timer timer1;
-        private Bunifu.UI.WinForms.BunifuGradientPanel bunifuGradientPanel1;
-        private System.Windows.Forms.Label label1;
-        private CircularProgressBar.CircularProgressBar progressbar;
     }
 }
-
--- Bus-Terminal-Management-System/bus-main/sr/Form1.cs
+++ Bus-Terminal-Management-System/bus-main/sr/Form1.cs
@@ -1,11 +1,6 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
 using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.Drawing.Drawing2D;
 using System.Windows.Forms;
 
 namespace sr
@@ -15,25 +10,204 @@ namespace sr
         public Form1()
         {
             InitializeComponent();
-            timer1.Start();
+
             progressbar.Value = 0;
-            
-            
+            progressbar.Text = "0%";
+            lblLoading.Text = "Preparing system...";
+
+            pictureBoxBus.Image = CreateBusTerminalImage(pictureBoxBus.Width, pictureBoxBus.Height);
+
+            timer1.Start();
         }
 
         private void timer1_Tick(object sender, EventArgs e)
         {
-            progressbar.Value = progressbar.Value + 1;
-            progressbar.Text = progressbar.Value + "%";
+            if (progressbar.Value < progressbar.Maximum)
+            {
+                progressbar.Value += 1;
+                progressbar.Text = progressbar.Value + "%";
+
+                if (progressbar.Value < 30)
+                {
+                    lblLoading.Text = "Loading database connection...";
+                }
+                else if (progressbar.Value < 60)
+                {
+                    lblLoading.Text = "Loading terminal modules...";
+                }
+                else if (progressbar.Value < 90)
+                {
+                    lblLoading.Text = "Preparing dashboard...";
+                }
+                else
+                {
+                    lblLoading.Text = "Almost ready...";
+                }
+            }
+
             if (progressbar.Value >= progressbar.Maximum)
             {
                 timer1.Stop();
-              
+
                 login log = new login();
                 this.Hide();
                 log.Show();
             }
+        }
 
+        private Image CreateBusTerminalImage(int width, int height)
+        {
+            Bitmap bmp = new Bitmap(width, height);
+
+            using (Graphics g = Graphics.FromImage(bmp))
+            {
+                g.SmoothingMode = SmoothingMode.AntiAlias;
+
+                using (LinearGradientBrush bg = new LinearGradientBrush(
+                    new Rectangle(0, 0, width, height),
+                    Color.FromArgb(235, 242, 250),
+                    Color.FromArgb(210, 228, 245),
+                    LinearGradientMode.Vertical))
+                {
+                    g.FillRectangle(bg, 0, 0, width, height);
+                }
+
+                // Sun / light
+                using (SolidBrush sunBrush = new SolidBrush(Color.FromArgb(255, 215, 95)))
+                {
+                    g.FillEllipse(sunBrush, width - 110, 35, 55, 55);
+                }
+
+                // Terminal building shadow
+                using (SolidBrush shadow = new SolidBrush(Color.FromArgb(55, 0, 0, 0)))
+                {
+                    g.FillRectangle(shadow, 52, 90, 365, 145);
+                }
+
+                // Terminal building
+                using (SolidBrush building = new SolidBrush(Color.FromArgb(32, 45, 64)))
+                {
+                    g.FillRectangle(building, 45, 80, 365, 145);
+                }
+
+                // Building roof
+                using (SolidBrush roof = new SolidBrush(Color.FromArgb(52, 152, 219)))
+                {
+                    Point[] roofPoints =
+                    {
+                        new Point(25, 80),
+                        new Point(225, 25),
+                        new Point(430, 80)
+                    };
+
+                    g.FillPolygon(roof, roofPoints);
+                }
+
+                // Terminal label board
+                using (SolidBrush board = new SolidBrush(Color.White))
+                {
+                    g.FillRoundedRectangle(board, 135, 92, 185, 38, 10);
+                }
+
+                using (Font font = new Font("Segoe UI", 14, FontStyle.Bold))
+                using (SolidBrush textBrush = new SolidBrush(Color.FromArgb(32, 45, 64)))
+                {
+                    StringFormat sf = new StringFormat();
+                    sf.Alignment = StringAlignment.Center;
+                    sf.LineAlignment = StringAlignment.Center;
+
+                    g.DrawString("BUS TERMINAL", font, textBrush, new RectangleF(135, 92, 185, 38), sf);
+                }
+
+                // Windows
+                using (SolidBrush window = new SolidBrush(Color.FromArgb(180, 220, 245)))
+                {
+                    g.FillRoundedRectangle(window, 75, 145, 55, 55, 8);
+                    g.FillRoundedRectangle(window, 150, 145, 55, 55, 8);
+                    g.FillRoundedRectangle(window, 255, 145, 55, 55, 8);
+                    g.FillRoundedRectangle(window, 330, 145, 55, 55, 8);
+                }
+
+                // Road
+                using (SolidBrush road = new SolidBrush(Color.FromArgb(58, 66, 78)))
+                {
+                    g.FillRectangle(road, 0, height - 95, width, 95);
+                }
+
+                // Road line
+                using (Pen roadLine = new Pen(Color.White, 4))
+                {
+                    roadLine.DashPattern = new float[] { 18, 15 };
+                    g.DrawLine(roadLine, 15, height - 48, width - 15, height - 48);
+                }
+
+                // Bus shadow
+                using (SolidBrush busShadow = new SolidBrush(Color.FromArgb(70, 0, 0, 0)))
+                {
+                    g.FillEllipse(busShadow, 90, height - 98, 320, 30);
+                }
+
+                // Bus body
+                using (SolidBrush busBody = new SolidBrush(Color.FromArgb(52, 152, 219)))
+                {
+                    g.FillRoundedRectangle(busBody, 75, height - 190, 335, 105, 20);
+                }
+
+                // Bus front
+                using (SolidBrush busFront = new SolidBrush(Color.FromArgb(41, 128, 185)))
+                {
+                    g.FillRoundedRectangle(busFront, 320, height - 190, 90, 105, 20);
+                }
+
+                // Bus windows
+                using (SolidBrush glass = new SolidBrush(Color.FromArgb(220, 245, 255)))
+                {
+                    g.FillRoundedRectangle(glass, 100, height - 170, 55, 38, 8);
+                    g.FillRoundedRectangle(glass, 165, height - 170, 55, 38, 8);
+                    g.FillRoundedRectangle(glass, 230, height - 170, 55, 38, 8);
+                    g.FillRoundedRectangle(glass, 330, height - 170, 50, 38, 8);
+                }
+
+                // Bus lower stripe
+                using (SolidBrush stripe = new SolidBrush(Color.White))
+                {
+                    g.FillRectangle(stripe, 90, height - 125, 285, 10);
+                }
+
+                // Bus door
+                using (Pen doorPen = new Pen(Color.White, 3))
+                {
+                    g.DrawLine(doorPen, 310, height - 178, 310, height - 95);
+                }
+
+                // Headlight
+                using (SolidBrush light = new SolidBrush(Color.FromArgb(255, 240, 120)))
+                {
+                    g.FillEllipse(light, 385, height - 120, 18, 12);
+                }
+
+                // Wheels
+                using (SolidBrush wheel = new SolidBrush(Color.FromArgb(25, 25, 25)))
+                {
+                    g.FillEllipse(wheel, 125, height - 105, 55, 55);
+                    g.FillEllipse(wheel, 310, height - 105, 55, 55);
+                }
+
+                using (SolidBrush rim = new SolidBrush(Color.FromArgb(210, 215, 220)))
+                {
+                    g.FillEllipse(rim, 140, height - 90, 25, 25);
+                    g.FillEllipse(rim, 325, height - 90, 25, 25);
+                }
+
+                // Decorative text
+                using (Font smallFont = new Font("Segoe UI", 10, FontStyle.Bold))
+                using (SolidBrush white = new SolidBrush(Color.White))
+                {
+                    g.DrawString("BTMS EXPRESS", smallFont, white, 105, height - 118);
+                }
+            }
+
+            return bmp;
         }
 
         private void Form1_Load(object sender, EventArgs e)
@@ -41,7 +215,7 @@ namespace sr
 
         }
 
-        private void pictureBox1_Click(object sender, EventArgs e)
+        private void pictureBoxBus_Click(object sender, EventArgs e)
         {
 
         }
@@ -49,6 +223,25 @@ namespace sr
         private void bunifuGradientPanel1_Click(object sender, EventArgs e)
         {
 
+        }
+    }
+
+    public static class GraphicsExtensions
+    {
+        public static void FillRoundedRectangle(this Graphics g, Brush brush, int x, int y, int width, int height, int radius)
+        {
+            using (GraphicsPath path = new GraphicsPath())
+            {
+                int diameter = radius * 2;
+
+                path.AddArc(x, y, diameter, diameter, 180, 90);
+                path.AddArc(x + width - diameter, y, diameter, diameter, 270, 90);
+                path.AddArc(x + width - diameter, y + height - diameter, diameter, diameter, 0, 90);
+                path.AddArc(x, y + height - diameter, diameter, diameter, 90, 90);
+                path.CloseFigure();
+
+                g.FillPath(brush, path);
+            }
         }
     }
 }
