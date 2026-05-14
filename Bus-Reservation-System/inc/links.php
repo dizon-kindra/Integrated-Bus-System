d5bb020b38e4e712b3529f6a9b437ebcce70d68f
dizon-kindra <dizonkinkin28@gamil.com>
2026-05-14 22:17:18 +0800

    Clean shared links include file

--- Bus-Reservation-System/inc/links.php
+++ Bus-Reservation-System/inc/links.php
@@ -1,3 +1,11 @@
+<?php
+if (session_status() === PHP_SESSION_NONE) {
+    session_start();
+}
+
+require_once(__DIR__ . '/essentials.php');
+?>
+
 <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet"
     integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
 
@@ -9,18 +17,8 @@
 
 <link rel="stylesheet" href="css/common.css">
 
-<link rel='stylesheet' href='https://cdn.jsdelivr.net/npm/swiper@11/swiper-bundle.min.css' />
+<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/swiper@11/swiper-bundle.min.css">
 
 <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
 
 <script src="https://www.google.com/recaptcha/api.js" async defer></script>
-
-
-<?php
-if (session_status() === PHP_SESSION_NONE) {
-    session_start();
-}
-
-require_once(__DIR__ . '/db_config.php');
-require_once(__DIR__ . '/essentials.php');
-?>
