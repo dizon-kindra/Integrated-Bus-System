7099f3621173635c1dad0dc81c3fb253cebf26e1
dizon-kindra <dizonkinkin28@gamil.com>
2026-05-14 15:17:23 +0800

    Fix include paths in passenger links

--- Bus-Reservation-System/inc/links.php
+++ Bus-Reservation-System/inc/links.php
@@ -21,7 +21,6 @@ if (session_status() === PHP_SESSION_NONE) {
     session_start();
 }
 
-require_once('admin/inc/db_config.php');
-require('admin/inc/essentials.php');
+require_once(__DIR__ . '/db_config.php');
+require_once(__DIR__ . '/essentials.php');
 ?>
-
